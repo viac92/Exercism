789633f1e33898ef9fba33d4dcebfabb64e4821b
viac92 <64146594+viac92@users.noreply.github.com>
2023-02-04 23:14:01 +0100

    resolved

--- fsharp/log-levels/LogLevels.fs
+++ fsharp/log-levels/LogLevels.fs
@@ -1,7 +1,15 @@
 module LogLevels
+open System.Text.RegularExpressions
 
-let message (logLine: string): string = failwith "Please implement the 'message' function"
 
-let logLevel(logLine: string): string = failwith "Please implement the 'logLevel' function"
+let message (logLine: string): string = 
+    let indexOfSemi = logLine.IndexOf(":")
+    let s = logLine.Substring(indexOfSemi + 2)
+    Regex.Replace(s, @"\t|\n|\r", "").Trim(' ')
 
-let reformat(logLine: string): string = failwith "Please implement the 'reformat' function"
+let logLevel(logLine: string): string = 
+    let IndexOfSemi = logLine.IndexOf(":")
+    logLine.Remove(IndexOfSemi).Trim('[', ']').ToLower()
+
+let reformat(logLine: string): string =
+    message(logLine) + " (" + logLevel(logLine) + ")"
