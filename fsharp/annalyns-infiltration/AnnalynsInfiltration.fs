1339f9311bd459c90c928facc7825b553c3152f5
viac92 <64146594+viac92@users.noreply.github.com>
2023-02-04 12:23:52 +0100

    first commit

--- fsharp/annalyns-infiltration/AnnalynsInfiltration.fs
+++ fsharp/annalyns-infiltration/AnnalynsInfiltration.fs
@@ -1,12 +1,12 @@
 module AnnalynsInfiltration
 
-let canFastAttack (knightIsAwake: bool): bool = failwith "Please implement the 'canFastAttack' function"
+let canFastAttack (knightIsAwake: bool): bool = not knightIsAwake
 
-let canSpy (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool): bool =
-    failwith "Please implement the 'canSpy' function"
+let canSpy (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool): bool = 
+    knightIsAwake || archerIsAwake || prisonerIsAwake
 
-let canSignalPrisoner (archerIsAwake: bool) (prisonerIsAwake: bool): bool =
-    failwith "Please implement the 'canSignalPrisoner' function"
+let canSignalPrisoner (archerIsAwake: bool) (prisonerIsAwake: bool): bool = 
+    not archerIsAwake && prisonerIsAwake
 
 let canFreePrisoner (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) (petDogIsPresent: bool): bool =
-    failwith "Please implement the 'canFreePrisoner' function"
+    not archerIsAwake && petDogIsPresent ||  not knightIsAwake && not archerIsAwake && prisonerIsAwake
